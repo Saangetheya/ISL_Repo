672371eec38d22f175fb043d0003b49ae80cff4c
Abhijith Shaji <88903559+Saangetheya@users.noreply.github.com>
2023-02-22 15:42:02 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -6,8 +6,6 @@ from torchvision.transforms import ToTensor
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SDG(model.parameters(),lr = 1e-3)
 
 classes = [
     "T-shirt/top",
